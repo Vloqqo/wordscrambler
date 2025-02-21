ce1e88eee6ea0ebe966065565a16167228b97095
Vloqqo <vloqqo@gmail.com>
2025-02-21 15:49:45 -0800

    Big Changes

--- stuff.js
+++ stuff.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'https://rcwwcczuieeqsqavydzo.supabase.co'
-const supabaseKey = ''
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjd3djY3p1aWVlcXNxYXZ5ZHpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzOTg3MTcsImV4cCI6MjA1NDk3NDcxN30.4RmiDtCajBUASsfAo2XnD0tQxMyTfK0QW_JpV10rqCg'
 const supabase = createClient(supabaseUrl, supabaseKey)
